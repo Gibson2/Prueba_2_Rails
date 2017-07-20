07fdfb060960ac60263ea9fddcfca713a845dc93
Guillermo <gibsonguillermoarturo@gmail.com>
2017-07-19 21:01:03 -0400

    fitth commit: Respondiendo la pregunta Scopes

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,8 @@
 class Item < ApplicationRecord
 	belongs_to :category
 	belongs_to :user, optional: true
+
+	scope :all_size_var, ->(var) { where("size=?", var) }
+	scope :item_with_no_user, -> { where(Item.user.nil?) }
+
 end
